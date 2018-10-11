7a4dd295b6ebe544f878a97ef184dbd48c027229
Jiangtao Xie <jiangtaoxie@mail.dlut.edu.cn>
2018-10-11 16:19:23 +0800

    Update resnet.py

--- PyTorch/imagenet/vision/torchvision/models/resnet.py
+++ PyTorch/imagenet/vision/torchvision/models/resnet.py
@@ -10,7 +10,7 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet50': 'https://drive.google.com/file/d/132PzY3eVDuGg8ROz5wON5FTC2E2o12Ck/view',
     'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
